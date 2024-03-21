dd0493aeb73a3d2094a127151ea2bea57402a3f1
TRO KOPE EMMANUEL JUNIOR <44979165+troemmanuel@users.noreply.github.com>
2024-03-21 07:46:16 +0100

    Update Parieur

--- src/main/java/com/trodieng/parisportif/model/Parieur.java
+++ src/main/java/com/trodieng/parisportif/model/Parieur.java
@@ -6,10 +6,14 @@ import org.openxava.model.*;
 
 import lombok.*;
 @Entity @Getter @Setter
-public class Parieur extends Identifiable  {
-	String nom;
+public class Parieur extends  Identifiable {
+	@Column
+	String name;
+	@Column
 	String email;
+	@Column
 	String motDePasse;
+	@Column
 	int jeton;
 
 }
