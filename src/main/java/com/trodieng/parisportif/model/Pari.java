c631533f3e74033e77ebdbd2c364e498cfdf7f5a
TRO KOPE EMMANUEL JUNIOR <44979165+troemmanuel@users.noreply.github.com>
2024-03-21 07:47:08 +0100

    Update classes

--- src/main/java/com/trodieng/parisportif/model/Pari.java
+++ src/main/java/com/trodieng/parisportif/model/Pari.java
@@ -2,11 +2,25 @@ package com.trodieng.parisportif.model;
 
 import javax.persistence.*;
 
+import org.openxava.annotations.*;
 import org.openxava.model.*;
 
 import lombok.*;
 @Entity @Getter @Setter
 public class Pari extends Identifiable  {
-	int montant;
+	@Column
+	private int montant;
+	@Column
+	private String resultat;
+	@ManyToOne( 
+	        fetch=FetchType.LAZY,
+	        optional=true) 
+	@DescriptionsList
+	Parieur parieur;
+	@ManyToOne( 
+	        fetch=FetchType.LAZY,
+	        optional=true)
+	@DescriptionsList
+	evenement evenement;
 
 }
--- src/main/java/com/trodieng/parisportif/model/evenement.java
+++ src/main/java/com/trodieng/parisportif/model/evenement.java
@@ -4,14 +4,24 @@ import java.time.*;
 
 import javax.persistence.*;
 
+import org.openxava.annotations.*;
 import org.openxava.model.*;
 
 import lombok.*;
 @Entity @Getter @Setter
 public class evenement extends Identifiable  {
-	private String label;
+	@Column
+	private String description;
+	@Column
 	private float cote;
+	@Column
     private String score;
+	@Column
     private LocalDate date;
+	@ManyToOne( 
+	        fetch=FetchType.LAZY,
+	        optional=true)
+	@DescriptionsList
+	Sport sport;
 
 }
